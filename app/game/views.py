1b541ad682ec9945b9f36a1b08c7860648ea7441
George Brata <bratageorge@gmail.com>
2016-03-24 13:15:54 +0200

    All blueprints required for app and HTML pages's structure added with new line at the end of each f file - FIXED

--- app/game/views.py
+++ app/game/views.py
@@ -3,5 +3,5 @@ from flask import Blueprint, render_template, redirect, url_for
 game = Blueprint('game', __name__)
 
 @game.route('/game')
-def gamee():
+def meatballs_game():
     return render_template('game/game.html')
