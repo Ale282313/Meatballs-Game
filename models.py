2a7786b03ebb53dfd8c14f7ec6efa87b4c2aebb2
Vlad Mihalache <contact@vladmihalache.ro>
2016-03-24 16:09:55 +0200

    Add models configuration for whole project.

--- models.py
+++ models.py
@@ -1,18 +1,50 @@
 from app import db
 from flask.ext.login import UserMixin
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, index=True)
     password = db.Column(db.String(60))
+    email = db.Column(db.String(120), unique=True)
+    first_name = db.Column(db.String(40))
+    second_name = db.Column(db.String(40))
+    games = db.relationship('Game',
+                            secondary='player',
+                            backref=db.backref('players', lazy='dynamic'),
+                            lazy='dynamic')
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, email, first_name, second_name):
         self.username = username
         self.password = password
+        self.email = email
+        self.first_name = first_name
+        self.second_name = second_name
 
     def __repr__(self):
-        return "User: {} {}".format(self.id, self.username)
+        return "user_id: {} , username: {}".format(self.id, self.username)
 
     def __str__(self):
-        return "User: {} {}".format(self.id, self.username)
+        return "user_id: {} , username: {}".format(self.id, self.username)
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    played_date = db.Column(db.DateTime, default=datetime.now())
+    winner_id = db.Column(db.Integer)
+
+    def __init__(self, winner_id, played_date=None):
+        self.winner_id = winner_id
+        self.played_date = played_date
+
+    def __repr__(self):
+        return "game_id: {} , winner_id: {}".format(self.id, self.winner_id)
+
+    def __str__(self):
+        return "game_id {} , winner_id: {}".format(self.id, self.winner_id)
+
+
+class Player(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'), primary_key=True)
