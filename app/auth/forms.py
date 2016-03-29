481e541ae9144cebbec6c832e7e4d05dda7a4e30
George Brata <bratageorge@gmail.com>
2016-03-29 10:59:52 +0300

    Register added and a few minor design changes for Login

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import DataRequired, Length, EqualTo, Regexp
 class RegisterForm(Form):
     username = StringField('Username:', validators=[
         DataRequired(message="Username required."),
-        Length(min=4, max=25,message="Sorry, not long enough...")
+        Length(min=4, max=25,message="Sorry, not long enough.")
     ])
 
     password = PasswordField('Password:', validators=[
@@ -20,25 +20,25 @@ class RegisterForm(Form):
 
     email = StringField('Email:', validators=[
         DataRequired(message="Email required."),
-        Length(min=4, max=25, message="Sorry, not long enough..."),
+        Length(min=4, max=25, message="Sorry, not long enough."),
         Regexp('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$',message="This doesn't look like an e-mail...")
     ])
 
     first_name = StringField('First Name:', validators=[
         DataRequired(message="First name required."),
-        Length(min=2, max=25,message="Sorry, not long enough...")
+        Length(min=2, max=25,message="Sorry, not long enough.")
     ])
 
     last_name = StringField('Last Name:', validators=[
         DataRequired(message="Last name required."),
-        Length(min=2, max=25,message="Sorry, not long enough...")
+        Length(min=2, max=25,message="Sorry, not long enough.")
     ])
 
 
 class LoginForm(Form):
     username = StringField('Username:', validators=[
         DataRequired(message="Username required."),
-        Length(min=4, max=25,message="Sorry, not long enough...")
+        Length(min=4, max=25,message="Sorry, not long enough.")
     ])
 
     password = PasswordField('Password:', validators=[
