42e6286d56cddfe8d86d0919a7a2649f8151ce6b
Vlad Mihalache <contact@vladmihalache.ro>
2016-03-30 16:48:01 +0300

    Fixed sprint1 menu links and css

--- app/game/views.py
+++ app/game/views.py
@@ -1,13 +1,16 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template
+from flask_login import login_required
 
 game = Blueprint('game', __name__)
 
 
 @game.route('/game')
+@login_required
 def meatballs_game():
     return render_template('game/game.html')
 
 
 @game.route('/after')
+@login_required
 def after():
     return render_template('game/after_game.html')
