19609a1ed0550d73b87fa360fccf7f6e8824eac2
Vlad Mihalache <contact@vladmihalache.ro>
2016-04-05 13:23:42 +0300

    Modified concat statement

--- app/static/js/socket_events.js
+++ app/static/js/socket_events.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var socket = io.connect('http://'.concat(document.domain).concat(':').concat(location.port).concat('/game'));
+    var socket = io.connect('http://'.concat(document.domain, ':', location.port, '/game'));
     socket.on('my response', function(msg) {
         $('#player-status').text(msg.data);
     });
