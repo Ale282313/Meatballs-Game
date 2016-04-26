fe4818612007b9afe48aef61ee88b175007a3c5c
George Brata <bratageorge@gmail.com>
2016-04-26 13:16:49 +0300

    Comments Fixed Fixed

--- app/game/views.py
+++ app/game/views.py
@@ -23,16 +23,16 @@ def after():
 @game.route('/statistics')
 @login_required
 def get_statistics():
-    winner = request.args.get('winner')
-    game_duration = request.args.get('game_duration')
-    player1_username = request.args.get('player1_username')
-    player1_total_shots = request.args.get('player1_totalShots')
-    player1_hit_shots = request.args.get('player1_hitShots')
+    winner = request.args.get('winner', default = "undefined")
+    game_duration = request.args.get('game_duration', default = 0)
+    player1_username = request.args.get('player1_username', default = "undefined")
+    player1_total_shots = request.args.get('player1_totalShots', default = 0)
+    player1_hit_shots = request.args.get('player1_hitShots', default = 0)
     player1_shield_activation = request.args.get('player1_shieldActivation')
-    player2_username = request.args.get('player2_username')
-    player2_total_shots = request.args.get('player2_totalShots')
-    player2_hit_shots = request.args.get('player2_hitShots')
-    player2_shield_activation = request.args.get('player2_shieldActivation')
+    player2_username = request.args.get('player2_username', default = "undefined")
+    player2_total_shots = request.args.get('player2_totalShots', default = 0)
+    player2_hit_shots = request.args.get('player2_hitShots', default = 0)
+    player2_shield_activation = request.args.get('player2_shieldActivation', default = 0)
 
 
     return redirect(url_for('game.after', winner=winner,
