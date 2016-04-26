a52df0c8954ac3c0ab73448a50efcdc31798bee8
Ale282313 <burnetealexandra@yahoo.com>
2016-04-27 01:28:28 +0300

    Fixed css, js and html problems

--- app/static/js/game/game.js
+++ app/static/js/game/game.js
@@ -73,22 +73,23 @@ $(document).ready(function () {
         
     socket.on('290', function(winnerUsername) {
         clearInterval(gameTimer);
-        showWarningMessage(winnerUsername + " won!")
+        showWarningMessage(winnerUsername + " won!");
         setTimeout(function(){
             socket.emit('290', winnerUsername);
         }, 3000);
     });
 
     socket.on('291', function(data) {
-        window.location = "./after?winner=" + data.winner
-            +"&game_duration=" + data.game_duration
-            +"&player1_username=" + data.player1_username
-            +"&player1_totalShots=" + data.player1_totalShots
-            +"&player1_hitShots=" + data.player1_hitShots
-            +"&player1_shieldActivation=" + data.player1_shieldActivation
-            +"&player2_username=" + data.player2_username
-            +"&player2_totalShots=" + data.player2_totalShots
-            +"&player2_hitShots=" + data.player2_hitShots
-            +"&player2_shieldActivation=" + data.player2_shieldActivation;
+        window.location = "";
+        window.location.concat("./after?winner=", data.winner,
+                               "&game_duration=", data.game_duration,
+                               "&player1_username=", data.player1_username,
+                               "&player1_totalShots=", data.player1_totalShots,
+                               "&player1_hitShots=", data.player1_hitShots,
+                               "&player1_shieldActivation=", data.player1_shieldActivation,
+                               "&player2_username=", data.player2_username,
+                               "&player2_totalShots=", data.player2_totalShots,
+                               "&player2_hitShots=", data.player2_hitShots,
+                               "&player2_shieldActivation=", data.player2_shieldActivation);
     });
 });
